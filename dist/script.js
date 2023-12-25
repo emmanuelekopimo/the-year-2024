042e8712ded2c140bb153ab8d6b8f200566797ad
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2023-12-25 04:15:45 +0100

    .

--- dist/script.js
+++ dist/script.js
@@ -6,15 +6,27 @@ const nSecond = document.querySelector(".n-sec");
 const nMs = document.querySelector(".n-ms");
 const title1 = document.querySelector(".title.first");
 const title2 = document.querySelector(".title.second");
-setInterval(() => {
-    let targetDate = new Date("12/31/2023");
-    let nowDate = new Date();
-    let intervalMS = targetDate.getTime() - nowDate.getTime();
-    let interval = new Date();
-    interval.setTime(intervalMS);
-    console.log(interval);
-    nDay.innerText = interval.getDate().toFixed();
-    nHour.innerText = interval.getHours().toFixed();
-    nMin.innerText = interval.getMinutes().toFixed();
-    nSecond.innerText = interval.getSeconds().toFixed();
-}, 500);
+const dCheck = new Date();
+if (dCheck.getFullYear() == 2024) {
+    title1.classList.toggle("hide", true);
+    title2.classList.toggle("hide", false);
+}
+else {
+    const hInt = setInterval(() => {
+        let targetDate = new Date("12/31/2023");
+        let nowDate = new Date();
+        if (nowDate.getFullYear() == 2024) {
+            title1.classList.toggle("hide", true);
+            title2.classList.toggle("hide", false);
+            clearInterval(hInt);
+        }
+        let intervalMS = targetDate.getTime() - nowDate.getTime();
+        let interval = new Date();
+        interval.setTime(intervalMS);
+        console.log(interval);
+        nDay.innerText = interval.getDate().toFixed();
+        nHour.innerText = interval.getHours().toFixed();
+        nMin.innerText = interval.getMinutes().toFixed();
+        nSecond.innerText = interval.getSeconds().toFixed();
+    }, 500);
+}
--- src/script.ts
+++ src/script.ts
@@ -6,15 +6,27 @@ const nMs: HTMLParagraphElement = document.querySelector(".n-ms")!;
 const title1 = document.querySelector(".title.first") as HTMLDivElement;
 const title2 = document.querySelector(".title.second") as HTMLDivElement;
 
-setInterval(() => {
-  let targetDate: Date = new Date("12/31/2023");
-  let nowDate: Date = new Date();
-  let intervalMS: number = targetDate.getTime() - nowDate.getTime();
-  let interval: Date = new Date();
-  interval.setTime(intervalMS);
-  console.log(interval);
-  nDay.innerText = interval.getDate().toFixed();
-  nHour.innerText = interval.getHours().toFixed();
-  nMin.innerText = interval.getMinutes().toFixed();
-  nSecond.innerText = interval.getSeconds().toFixed();
-}, 500);
+const dCheck: Date = new Date();
+
+if (dCheck.getFullYear() == 2024) {
+  title1.classList.toggle("hide", true);
+  title2.classList.toggle("hide", false);
+} else {
+  const hInt = setInterval(() => {
+    let targetDate: Date = new Date("12/31/2023");
+    let nowDate: Date = new Date();
+    if (nowDate.getFullYear() == 2024) {
+      title1.classList.toggle("hide", true);
+      title2.classList.toggle("hide", false);
+      clearInterval(hInt);
+    }
+    let intervalMS: number = targetDate.getTime() - nowDate.getTime();
+    let interval: Date = new Date();
+    interval.setTime(intervalMS);
+    console.log(interval);
+    nDay.innerText = interval.getDate().toFixed();
+    nHour.innerText = interval.getHours().toFixed();
+    nMin.innerText = interval.getMinutes().toFixed();
+    nSecond.innerText = interval.getSeconds().toFixed();
+  }, 500);
+}
